af1df9043ddb9ddda522a7736f834f7548efe1f0
양정훈 <yjhoon3658@naver.com>
2023-10-27 17:13:02 +0900

    :sparkles: Feat: ProofImage 엔티티 구현

---
+++ proof/src/main/java/com/eokam/proof/domain/entity/ProofImage.java
@@ -0,0 +1,34 @@
+package com.eokam.proof.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ProofImage {
+	@Id
+	@GeneratedValue
+	Long proofPhotoId;
+
+	@ManyToOne
+	@JoinColumn(name = "proofId")
+	Proof proof;
+
+	@Column(nullable = false)
+	String fileName;
+
+	@Column(nullable = false)
+	String fileUrl;
+}
