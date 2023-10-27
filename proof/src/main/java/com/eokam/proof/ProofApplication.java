d5fe6969ad4a9ff3c7b5e378893a65675a9c4323
양정훈 <yjhoon3658@naver.com>
2023-10-27 17:26:24 +0900

    :building_construction: Settings: JPA CreatedDate를 적용하기 위해 EnableJpaAudting 애노테이션 적용

--- proof/src/main/java/com/eokam/proof/ProofApplication.java
+++ proof/src/main/java/com/eokam/proof/ProofApplication.java
@@ -2,8 +2,10 @@ package com.eokam.proof;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ProofApplication {
 
 	public static void main(String[] args) {
