f5e75e4bd2f50084b5ec6baf2c7af8403adb8073
이상재 <tkdwo7699@gmail.com>
2023-10-27 17:36:42 +0900

    ✅Test: 탄소중립실천포인트 조회 인수테스트 생성

---
+++ cpoint/src/test/java/com/eokam/cpoint/acceptance/CpointSteps.java
@@ -0,0 +1,30 @@
+package com.eokam.cpoint.acceptance;
+
+import com.eokam.cpoint.presentation.dto.CpointCreateRequest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CpointSteps {
+    public static ExtractableResponse<Response> 탄소중립실천포인트를_조회하면(String JWT_쿠키){
+        return RestAssured.given()
+                .cookie(JWT_쿠키)
+                .when()
+                .get("/cpoint")
+                .then()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 탄소중립실천포인트가_적립됨(final CpointCreateRequest 생성요청){
+        return RestAssured.given()
+                .body(생성요청)
+                .when()
+                .post("/cpoint")
+                .then()
+                .extract();
+    }
+
+
+}
