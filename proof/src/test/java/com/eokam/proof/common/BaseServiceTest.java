f4f0d6f924bee19e9c29b5669f461bf7d3c8401a
양정훈 <yjhoon3658@naver.com>
2023-10-30 13:57:55 +0900

    :white_check_mark: Test: 내 인증 내역 Service 단위 테스트 작성

---
+++ proof/src/test/java/com/eokam/proof/common/BaseServiceTest.java
@@ -0,0 +1,13 @@
+package com.eokam.proof.common;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith({MockitoExtension.class, SpringExtension.class})
+@SpringBootTest
+@Disabled
+public class BaseServiceTest {
+}
