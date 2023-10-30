e62a28d59feecc888e1a8d8a3a13ab1164b7b026
jxixeun <jxixeun@gmail.com>
2023-10-30 15:04:15 +0900

    :sparkles: Feat: 값을 전달하기 위한 AccusationDto 생성

---
+++ accusation/src/main/java/com/eokam/accusation/application/dto/AccusationDto.java
@@ -0,0 +1,34 @@
+package com.eokam.accusation.application.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.eokam.accusation.domain.entity.Accusation;
+import com.eokam.accusation.global.constant.ActivityType;
+import com.eokam.accusation.presentation.dto.AccusationRequest;
+
+import lombok.Builder;
+
+@Builder
+public record AccusationDto(Long accusationId, Long witnessId, Long memberId, ActivityType activityType,
+							String activityDetail, List<String> imageList, LocalDateTime createdAt) {
+	public static AccusationDto of(AccusationRequest request) {
+		return AccusationDto.builder()
+			.witnessId(request.getWitnessId())
+			.memberId(request.getMemberId())
+			.activityType(request.getActivityType())
+			.activityDetail(request.getActivityDetail())
+			.build();
+	}
+
+	public static AccusationDto of(Accusation accusation, List<String> fileUrls) {
+		return AccusationDto.builder()
+			.accusationId(accusation.getAccusationId())
+			.witnessId(accusation.getWitnessId())
+			.memberId(accusation.getMemberId())
+			.activityType(accusation.getActivityType())
+			.activityDetail(accusation.getActivityDetail())
+			.imageList(fileUrls)
+			.build();
+	}
+}
