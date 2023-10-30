33541b9578998a388ad841be12282685b5051749
양정훈 <yjhoon3658@naver.com>
2023-10-30 16:34:18 +0900

    :bug: fix: JPA 옵션 변경

--- proof/src/main/java/com/eokam/proof/domain/entity/ProofImage.java
+++ proof/src/main/java/com/eokam/proof/domain/entity/ProofImage.java
@@ -2,6 +2,7 @@ package com.eokam.proof.domain.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -22,7 +23,7 @@ public class ProofImage {
 	@GeneratedValue
 	Long proofImageId;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "proofId")
 	Proof proof;
 
