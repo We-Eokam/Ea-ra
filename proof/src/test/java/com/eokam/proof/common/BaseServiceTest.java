fcf64c34803dd30cd1d198c160c433a9071eec48
양정훈 <yjhoon3658@naver.com>
2023-10-30 15:07:26 +0900

    :heavy_plus_sign: Dependencies: Test 용 database 환경 구축

--- proof/src/test/java/com/eokam/proof/common/BaseServiceTest.java
+++ proof/src/test/java/com/eokam/proof/common/BaseServiceTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith({MockitoExtension.class, SpringExtension.class})
 @SpringBootTest
+@ActiveProfiles("test")
 @Disabled
 public class BaseServiceTest {
 }
