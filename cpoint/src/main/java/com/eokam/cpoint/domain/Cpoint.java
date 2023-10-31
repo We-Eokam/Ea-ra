ec3c4242992096a7e96de5c31038cc0a1c2b19ed
이상재 <tkdwo7699@gmail.com>
2023-10-31 14:51:09 +0900

    ✨Feat: 탄소중립실천포인트적립내역 엔티티 생성

---
+++ cpoint/src/main/java/com/eokam/cpoint/domain/Cpoint.java
@@ -0,0 +1,41 @@
+package com.eokam.cpoint.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Cpoint extends BaseEntity {
+
+	@Id
+	@Column(name = "cpoint_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private Integer point;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private ActivityType activityType;
+
+	@Column(nullable = false)
+	private Long memberId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "company_id")
+	private Company company;
+
+}
