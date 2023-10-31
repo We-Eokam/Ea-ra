0f06578e6ba907cd7cdab013889459481ff06e8b
양정훈 <yjhoon3658@naver.com>
2023-10-31 10:00:42 +0900

    :white_check_mark: Test: 테스트 시작 전 데이터베이스 초기화 로직 구현

---
+++ proof/src/test/java/com/eokam/proof/util/DatabaseCleanUp.java
@@ -0,0 +1,28 @@
+package com.eokam.proof.util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Component
+public class DatabaseCleanUp {
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	private void truncate() {
+		String[] tableNames = {"accusation", "accusation_image"};
+		entityManager.createNativeQuery(String.format("SET FOREIGN_KEY_CHECKS = %d", 0)).executeUpdate();
+		for (String tableName : tableNames) {
+			entityManager.createNativeQuery(String.format("TRUNCATE TABLE %s", tableName)).executeUpdate();
+		}
+		entityManager.createNativeQuery(String.format("SET FOREIGN_KEY_CHECKS = %d", 1)).executeUpdate();
+	}
+
+	@Transactional
+	public void clear() {
+		entityManager.clear();
+		truncate();
+	}
+}
