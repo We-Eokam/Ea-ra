e893c120106caf622a283ab5cc61f736f4db1e75
양정훈 <yjhoon3658@naver.com>
2023-10-31 10:03:08 +0900

    :white_check_mark: Test: 테스트 시작 전 데이터베이스 초기화 설정

--- proof/src/test/java/com/eokam/proof/common/AcceptanceTest.java
+++ proof/src/test/java/com/eokam/proof/common/AcceptanceTest.java
@@ -2,13 +2,19 @@ package com.eokam.proof.common;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.eokam.proof.util.DatabaseCleanUpExtension;
 
 import io.restassured.RestAssured;
 
+@ExtendWith(DatabaseCleanUpExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Disabled
+@ActiveProfiles("test")
 public class AcceptanceTest {
 
 	@LocalServerPort
--- proof/src/test/java/com/eokam/proof/common/BaseControllerTest.java
+++ proof/src/test/java/com/eokam/proof/common/BaseControllerTest.java
@@ -3,23 +3,35 @@ package com.eokam.proof.common;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.restdocs.RestDocsAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 import com.eokam.proof.util.DatabaseCleanUpExtension;
 
 import io.restassured.RestAssured;
 
-@ExtendWith({MockitoExtension.class, SpringExtension.class, DatabaseCleanUpExtension.class})
+@ExtendWith({SpringExtension.class, DatabaseCleanUpExtension.class})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@Import(RestDocsAutoConfiguration.class)
 @ActiveProfiles("test")
 @Disabled
-public class BaseServiceTest {
+public class BaseControllerTest {
+
 	@LocalServerPort
 	int port;
+
+	@Autowired
+	protected MockMvc mockMvc;
 
 	@BeforeEach
 	public void setUp() {
