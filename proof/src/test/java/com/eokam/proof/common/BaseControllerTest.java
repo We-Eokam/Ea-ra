b865038a0a424677f4c319546fee7465690b6ca4
양정훈 <yjhoon3658@naver.com>
2023-11-01 14:45:35 +0900

    :art: Refactor: Controller 테스트 및 코드 리팩토링

--- proof/src/test/java/com/eokam/proof/common/BaseControllerTest.java
+++ proof/src/test/java/com/eokam/proof/common/BaseControllerTest.java
@@ -1,40 +1,18 @@
 package com.eokam.proof.common;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.restdocs.RestDocsAutoConfiguration;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.eokam.proof.util.DatabaseCleanUpExtension;
-
-import io.restassured.RestAssured;
-
-@ExtendWith({SpringExtension.class, DatabaseCleanUpExtension.class})
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith({MockitoExtension.class})
 @AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsAutoConfiguration.class)
 @ActiveProfiles("test")
 @Disabled
 public class BaseControllerTest {
 
-	@LocalServerPort
-	int port;
-
-	@Autowired
 	protected MockMvc mockMvc;
 
-	@BeforeEach
-	public void setUp() {
-		RestAssured.port = port;
-	}
 }
