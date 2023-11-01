76528f931a56d23307565b4024684f4f749c1a7e
jxixeun <jxixeun@gmail.com>
2023-11-01 18:06:42 +0900

    🧪 Test: 받은 고발장 목록 조회 인수 테스트 작성

--- accusation/src/test/java/com/eokam/accusation/acceptance/AccusationAcceptanceTest.java
+++ accusation/src/test/java/com/eokam/accusation/acceptance/AccusationAcceptanceTest.java
@@ -3,9 +3,12 @@ package com.eokam.accusation.acceptance;
 import static io.restassured.RestAssured.*;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -34,6 +37,9 @@ public class AccusationAcceptanceTest {
 	private static final long MEMBER_ID = 2L;
 	private static final ActivityType ACTIVITY_TYPE = ActivityType.PLASTIC;
 
+	private static final String FILE_NAME1 = "fileName1.png";
+	private static final String FILE_NAME2 = "fileName2.png";
+
 	@LocalServerPort
 	int port;
 
@@ -46,10 +52,38 @@ public class AccusationAcceptanceTest {
 	@DisplayName("고발장을 갖고 있는 회원은 친구에게 고발장을 보낼 수 있다.")
 	void createAccusation() throws IOException {
 		// when
-		ExtractableResponse<Response> response = 고발장_생성_요청(고발장_요청_JSON(), 고발장_요청_사진());
+		ExtractableResponse<Response> response = 고발장_생성_요청(고발장_요청_JSON(), 고발장_요청_사진(FILE_NAME1));
 
 		// then
 		고발장_생성됨(response);
+	}
+
+	@Test
+	@Disabled
+	@DisplayName("회원은 자신이 받은 고발장 목록을 조회할 수 있다.")
+	void getMyAccusations() throws IOException {
+		// given
+		ExtractableResponse<Response> accusation1 = 고발장_생성되어_있음(FILE_NAME1);
+		ExtractableResponse<Response> accusation2 = 고발장_생성되어_있음(FILE_NAME2);
+
+		// when
+		ExtractableResponse<Response> response = 받은_고발장_목록_조회_요청();
+
+		// then
+		받은_고발장_목록_응답됨(response);
+		받은_고발장_목록_포함됨(response, Arrays.asList(accusation1, accusation2));
+	}
+
+	public static ExtractableResponse<Response> 고발장_생성되어_있음(String fileName) throws IOException {
+		return 고발장_생성_요청(고발장_요청_JSON(), 고발장_요청_사진(fileName));
+	}
+
+	public static ExtractableResponse<Response> 받은_고발장_목록_조회_요청() throws IOException {
+		return given().log().all()
+			.header("Authorization", MEMBER_ID)
+			.when().get("/accusation")
+			.then().log().all()
+			.extract();
 	}
 
 	public static String 고발장_요청_JSON() throws JsonProcessingException {
@@ -63,8 +97,8 @@ public class AccusationAcceptanceTest {
 		return objectMapper.writeValueAsString(accusationRequest);
 	}
 
-	public static MockMultipartFile 고발장_요청_사진() {
-		return new MockMultipartFile("file", "filename.png", "image/png", "image".getBytes());
+	public static MockMultipartFile 고발장_요청_사진(String fileName) {
+		return new MockMultipartFile("file", fileName, "image/png", fileName.getBytes());
 	}
 
 	public static void 고발장_생성됨(ExtractableResponse<Response> response) {
@@ -81,6 +115,22 @@ public class AccusationAcceptanceTest {
 			.when().post("/accusation")
 			.then().log().all()
 			.extract();
+	}
+
+	public static void 받은_고발장_목록_응답됨(ExtractableResponse<Response> response) {
+		Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+	}
+
+	public static void 받은_고발장_목록_포함됨(ExtractableResponse<Response> response,
+		List<ExtractableResponse<Response>> createdResponses) {
+
+		List<Long> expectedAccusationIds = createdResponses.stream()
+			.map(r -> Long.parseLong(r.header("Location").split("/")[2]))
+			.toList();
+
+		List<Long> resultAccusationIds = response.jsonPath().getList("accusation_list.accusation_id");
+
+		Assertions.assertThat(resultAccusationIds).containsAll(expectedAccusationIds);
 	}
 
 }
