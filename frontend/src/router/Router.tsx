2b62d43a1a545a1819d9f8736da738fce02f6e2f
gus991121 <gus991121@naver.com>
2023-11-02 00:33:43 +0900

    :sparkles: Feat: 제보화면 레이아웃 구현

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -6,6 +6,7 @@ import NotiPage from "../pages/NotiPage/NotiPage";
 import FeedPage from "../pages/FeedPage/FeedPage";
 import ActPage from "../pages/ActPage/ActPage";
 import PostPage from "../pages/ActPage/PostPage";
+import ReportPage from "../pages/ActPage/ReportPage";
 import NtzPage from "../pages/NtzPage/NtzPage";
 import MyPage from "../pages/MyPage/MyPage";
 
@@ -26,6 +27,7 @@ export default function Router() {
         <Route path="/feed" element={<FeedPage />}></Route>
         <Route path="/act" element={<ActPage />}></Route>
         <Route path="/act/post" element={<PostPage />}></Route>
+        <Route path="/act/report" element={<ReportPage />}></Route>
         <Route path="/netzero" element={<NtzPage />}></Route>
         <Route path="/netzero/subsidy" element={<Subsidy />}></Route>
         <Route path="/netzero/map" element={<MapPage />}></Route>
