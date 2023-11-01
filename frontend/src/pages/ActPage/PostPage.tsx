4518607df61d31e6272fb3ccc3cf76c420b646bc
윤지현 <gus991121@naver.com>
2023-11-01 17:33:18 +0900

    :sparkles: Feat: 활동 인증 시 기업 선택 구현

--- frontend/src/pages/ActPage/PostPage.tsx
+++ frontend/src/pages/ActPage/PostPage.tsx
@@ -1,13 +1,26 @@
 // import React from 'react'
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import HeadBar from "../../components/HeadBar/HeadBar";
 import MainFrame from "../../components/MainFrame/MainFrame";
 import styled from "styled-components";
 import ImageCropper from "../../components/ImageCropper/ImageCropper";
+import { ShortButton } from "../../components/Buttons/ShortButton";
+import { LongButton } from "../../components/Buttons/LongButton";
 
 export default function PostPage() {
   const [type, setType] = useState(0);
   const [croppedImage, setCroppedImage] = useState<string | null>(null);
+  const [isRegist, setIsRegist] = useState(false);
+  const companys = [
+    "더벤티",
+    "메가커피",
+    "스타벅스",
+    "풀 바셋",
+    "해피해빗",
+    "그린업",
+    "(주)다와",
+  ];
+  const [companyIdx, setCompanyIdx] = useState<number | null>(null);
 
   useEffect(() => {
     const params = new URLSearchParams(location.search);
@@ -22,18 +35,65 @@ export default function PostPage() {
     setCroppedImage(image);
   };
 
+  const handleRegist = (select: boolean) => {
+    setIsRegist(select);
+    if (select === false) {
+      setCompanyIdx(null);
+    }
+  };
+
   return (
     <>
       <HeadBar pagename="" bgcolor="white" backbutton="yes" />
       <MainFrame headbar="yes" navbar="no" bgcolor="white" marginsize="large">
-        <div style={{ margin: "1px" }} />
-        <ImageCropper onCrop={handleImageCrop}>
-          {croppedImage ? (
-            <CropImg src={croppedImage} alt="Cropped" />
-          ) : (
-            <ImgIcon src="../src/assets/images/upload-image-icon.png" />
-          )}
-        </ImageCropper>
+        <div style={{ marginTop: "20px" }}>
+          <ImageCropper onCrop={handleImageCrop}>
+            {croppedImage ? (
+              <CropImg src={croppedImage} alt="Cropped" />
+            ) : (
+              <ImgIcon src="../src/assets/images/upload-image-icon.png" />
+            )}
+          </ImageCropper>
+        </div>
+        <InfoFrame>
+          <InfoName>인증 활동</InfoName>
+          드롭다운 들어갈 자리
+        </InfoFrame>
+        <InfoFrame>
+          <InfoName>참여 기업 등록</InfoName>
+          <ButtonsFrame>
+            <Buton
+              style={{ width: "47.2%" }}
+              onClick={() => handleRegist(false)}
+              isSelected={isRegist === false}
+            >
+              아니요
+            </Buton>
+            <Buton
+              style={{ width: "47.2%" }}
+              onClick={() => handleRegist(true)}
+              isSelected={isRegist === true}
+            >
+              예
+            </Buton>
+          </ButtonsFrame>
+        </InfoFrame>
+        {isRegist && (
+          <InfoFrame>
+            <InfoName>기업선택</InfoName>
+            <ButtonsFrame>
+              {companys.map((company, idx) => (
+                <Buton
+                  onClick={() => setCompanyIdx(idx)}
+                  isSelected={companyIdx === idx}
+                >
+                  {company}
+                </Buton>
+              ))}
+            </ButtonsFrame>
+          </InfoFrame>
+        )}
+        <LongButton>인증하기</LongButton>
       </MainFrame>
     </>
   );
@@ -48,4 +108,35 @@ const CropImg = styled.img`
 const ImgIcon = styled.img`
   width: 36%;
   padding: 32%;
+`;
+
+const InfoFrame = styled.div`
+  width: 100%;
+  margin: 32px 0;
+`;
+
+const InfoName = styled.div`
+  position: relative;
+  width: 100%;
+  font-size: 14px;
+  color: var(--dark-gray);
+`;
+
+const ButtonsFrame = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  flex-flow: wrap;
+`;
+
+const Buton = styled(ShortButton)<{ isSelected: boolean }>`
+  width: 30%;
+  border: ${(props) =>
+    props.isSelected ? "1px solid transparent" : "1px solid var(--nav-gray)"};
+  color: ${(props) => (props.isSelected ? "var(--white)" : "var(--nav-gray)")};
+  background-color: ${(props) =>
+    props.isSelected ? "var(--primary)" : "var(--white)"};
+  height: 34px;
+  margin-top: 8px;
 `;
