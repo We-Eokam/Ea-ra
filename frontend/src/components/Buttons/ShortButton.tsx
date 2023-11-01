6a725a08fb6c7b02b403b6d4311914f381e37396
임준수 <yimj57027@hotmail.com>
2023-11-01 15:10:59 +0900

    :sparkles: Feat: 회원가입 정보입력 페이지

--- frontend/src/components/Buttons/ShortButton.tsx
+++ frontend/src/components/Buttons/ShortButton.tsx
@@ -3,13 +3,13 @@ import styled from "styled-components"
 export const ShortButton = styled.div`
   position: relative;
   width: 100px;
-  height: 38px;
+  height: 42px;
   background-color: var(--primary);
   color: var(--white);
   border-radius: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 12.5px;
+  font-size: 13.5px;
   font-weight: 400;
 `
