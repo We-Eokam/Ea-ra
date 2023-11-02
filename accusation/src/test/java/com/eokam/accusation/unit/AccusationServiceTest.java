832351ff16a9de5886769d74cec85c5c80a4dc93
jxixeun <jxixeun@gmail.com>
2023-11-02 16:01:27 +0900

    :white_check_mark: Test: 고발장 목록 조회 서비스 레이어 단위테스트 작성

--- accusation/src/test/java/com/eokam/accusation/unit/AccusationServiceTest.java
+++ accusation/src/test/java/com/eokam/accusation/unit/AccusationServiceTest.java
@@ -4,14 +4,17 @@ import static org.mockito.ArgumentMatchers.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
@@ -81,5 +84,57 @@ public class AccusationServiceTest {
 			.save(argThat(a -> a.getActivityDetail().equals("Test Detail")));
 		BDDMockito.verify(accusationImageRepository)
 			.save((argThat(aI -> aI.getFileUrl().equals("FileURL"))));
+	}
+
+	@Test
+	@DisplayName("받은 고발장 목록을 조회한다.")
+	void getAccusationList() {
+		// given
+		List<Accusation> accusations = number수만큼_memberId에게_고발장_보내기(1L, 10);
+		List<AccusationImage> accusationImages = 보낸_고발장에_사진_한개씩_추가(accusations);
+		BDDMockito.given(accusationRepository.findByMemberId(1L)).willReturn(accusations);
+		for (int i = 1; i <= 10; i++) {
+			BDDMockito.given(accusationImageRepository.findByAccusation_AccusationId((long)i)).willReturn(
+				Collections.singletonList(accusationImages.get(i - 1)));
+		}
+
+		// when
+		List<AccusationDto> accusationDtoList = accusationService.getAccusationList(1L);
+
+		// then
+		BDDMockito.verify(accusationRepository).findByMemberId(1L);
+		for (int i = 1; i <= 10; i++) {
+			BDDMockito.verify(accusationImageRepository).findByAccusation_AccusationId((long)i);
+		}
+		BDDMockito.verify(accusationImageRepository, Mockito.times(10))
+			.findByAccusation_AccusationId(any());
+		Assertions.assertThat(accusationDtoList).hasSize(10);
+		for (AccusationDto accusationDto : accusationDtoList) {
+			Assertions.assertThat(accusationDto.imageList()).hasSize(1);
+		}
+	}
+
+	public List<Accusation> number수만큼_memberId에게_고발장_보내기(Long memberId, int number) {
+		List<Accusation> accusations = new ArrayList<>();
+		for (int i = 1; i <= number; i++) {
+			accusations.add(Accusation.builder()
+				.accusationId((long)i)
+				.witnessId((long)i)
+				.memberId(memberId)
+				.activityType(ActivityType.FOOD)
+				.build());
+		}
+		return accusations;
+	}
+
+	public List<AccusationImage> 보낸_고발장에_사진_한개씩_추가(List<Accusation> accusations) {
+		List<AccusationImage> accusationImages = new ArrayList<>();
+		for (Accusation accusation : accusations) {
+			accusationImages.add(AccusationImage.builder()
+				.accusation(accusation)
+				.fileUrl("fileURL")
+				.build());
+		}
+		return accusationImages;
 	}
 }
