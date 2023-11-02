f21c9ae3134b1dcfafad12e18b7b187a75bca2b8
양정훈 <yjhoon3658@naver.com>
2023-11-02 14:57:06 +0900

    :sparkles: Feat: S3 저장 에러 코드 추가

--- proof/src/main/java/com/eokam/proof/infrastructure/util/error/ErrorCode.java
+++ proof/src/main/java/com/eokam/proof/infrastructure/util/error/ErrorCode.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-	;
+	S3_STORE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S3_001", "사진을 저장하는 동안 에러가 발생했습니다.");
 
 	ErrorCode(HttpStatus httpStatus, String errorCode, String message) {
 		this.httpStatus = httpStatus;
