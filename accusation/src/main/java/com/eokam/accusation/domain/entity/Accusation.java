a4cd808b1c5ba21bc1343195ba01456bca3cf436
jxixeun <jxixeun@gmail.com>
2023-11-02 15:20:33 +0900

    :pencil2: Fix: 양방향 연관 관계 단방향으로 변경

--- accusation/src/main/java/com/eokam/accusation/domain/entity/Accusation.java
+++ accusation/src/main/java/com/eokam/accusation/domain/entity/Accusation.java
@@ -1,8 +1,6 @@
 package com.eokam.accusation.domain.entity;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,7 +8,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import com.eokam.accusation.application.dto.AccusationDto;
 import com.eokam.accusation.global.constant.ActivityType;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
@@ -19,7 +16,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -48,9 +44,6 @@ public class Accusation {
 	private ActivityType activityType;
 
 	private String activityDetail;
-
-	@OneToMany(mappedBy = "accusation", cascade = CascadeType.REMOVE, orphanRemoval = true)
-	private List<AccusationImage> imageList = new ArrayList<>();
 
 	@CreatedDate
 	@Column(nullable = false)
