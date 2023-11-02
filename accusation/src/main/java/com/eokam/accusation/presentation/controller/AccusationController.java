baf8aba6268b43284a5772a9e205fdf51004b8a8
jxixeun <jxixeun@gmail.com>
2023-11-02 13:33:02 +0900

    :sparkles: Feat: 고발 목록을 반환하는 컨트롤러 메서드 구현

--- accusation/src/main/java/com/eokam/accusation/presentation/controller/AccusationController.java
+++ accusation/src/main/java/com/eokam/accusation/presentation/controller/AccusationController.java
@@ -5,14 +5,17 @@ import java.util.List;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.eokam.accusation.application.dto.AccusationDto;
 import com.eokam.accusation.application.service.AccusationService;
+import com.eokam.accusation.presentation.dto.AccusationListResponse;
 import com.eokam.accusation.presentation.dto.AccusationRequest;
 import com.eokam.accusation.presentation.dto.AccusationResponse;
 
@@ -32,5 +35,11 @@ public class AccusationController {
 		AccusationDto accusationDto = accusationService.createAccusation(AccusationDto.of(request), images);
 		AccusationResponse response = AccusationResponse.from(accusationDto);
 		return ResponseEntity.created(URI.create("/accusation/" + response.getAccusationId())).body(response);
+	}
+
+	@GetMapping
+	public ResponseEntity<?> getAccusationList(@RequestParam Long memberId) {
+		List<AccusationDto> accusationDtoList = accusationService.getAccusationList(memberId);
+		return ResponseEntity.ok(AccusationListResponse.from(accusationDtoList));
 	}
 }
