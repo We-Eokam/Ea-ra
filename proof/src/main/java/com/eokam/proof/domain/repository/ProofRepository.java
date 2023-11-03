72afb51b2513de0f316714716ed615a67f7a27d6
양정훈 <yjhoon3658@naver.com>
2023-11-03 11:04:53 +0900

    :sparkles: Feat: ProofId를 통해 proof를 조회하는 메서드 작성

--- proof/src/main/java/com/eokam/proof/domain/repository/ProofRepository.java
+++ proof/src/main/java/com/eokam/proof/domain/repository/ProofRepository.java
@@ -1,5 +1,7 @@
 package com.eokam.proof.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +12,6 @@ import com.eokam.proof.domain.entity.Proof;
 @Repository
 public interface ProofRepository extends JpaRepository<Proof, Long> {
 	Page<Proof> findAllByMemberId(Long memberId, Pageable pageable);
+
+	Optional<Proof> findByProofId(Long proofId);
 }
