f5704743935bdc32cb1d9d7227fd5537a6952736
jxixeun <jxixeun@gmail.com>
2023-11-03 12:52:14 +0900

    :tada: Init: 프로젝트 생성

---
+++ groo/src/main/java/com/eokam/groo/GrooApplication.java
@@ -0,0 +1,13 @@
+package com.eokam.groo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrooApplication.class, args);
+	}
+
+}
---
+++ groo/src/test/java/com/eokam/groo/GrooApplicationTests.java
@@ -0,0 +1,15 @@
+package com.eokam.groo;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrooApplicationTests {
+
+	@Test
+	@Disabled
+	void contextLoads() {
+	}
+
+}
