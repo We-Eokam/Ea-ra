e4f834048a099a68e80c2c7cca7dcef3cc062746
jxixeun <jxixeun@gmail.com>
2023-11-06 17:55:55 +0900

    :sparkles: Feat: 특정 월의 일별 그루 적립 양과 횟수를 조회하는 서비스 메서드 구현

--- groo/src/main/java/com/eokam/groo/application/service/GrooSavingServiceImpl.java
+++ groo/src/main/java/com/eokam/groo/application/service/GrooSavingServiceImpl.java
@@ -1,9 +1,14 @@
 package com.eokam.groo.application.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.eokam.groo.application.dto.GrooMonthDto;
 import com.eokam.groo.application.dto.GrooSavingDto;
 import com.eokam.groo.domain.entity.GrooSaving;
+import com.eokam.groo.infrastructure.dto.GrooDailySumAmountDto;
+import com.eokam.groo.infrastructure.dto.GrooMonthSumAmountDto;
 import com.eokam.groo.infrastructure.repository.GrooSavingRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -18,5 +23,13 @@ public class GrooSavingServiceImpl implements GrooSavingService{
 	public GrooSavingDto createGrooSaving(GrooSavingDto grooSavingDto) {
 		GrooSaving grooSaving = grooSavingRepository.save(GrooSaving.from(grooSavingDto));
 		return GrooSavingDto.from(grooSaving);
+	}
+
+	@Override
+	public GrooMonthDto getDailySavingAmountByMonth(Long memberId, Integer year, Integer month) {
+		List<GrooDailySumAmountDto> dailySumAndAmount = grooSavingRepository.getDailySumAndAmount(memberId, year,
+			month);
+		GrooMonthSumAmountDto grooMonthSumAmountDto = grooSavingRepository.getSumAndAmountByMonth(memberId, year, month);
+		return GrooMonthDto.of(dailySumAndAmount, grooMonthSumAmountDto);
 	}
 }
