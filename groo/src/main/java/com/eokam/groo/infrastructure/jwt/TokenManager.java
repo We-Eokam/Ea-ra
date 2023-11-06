50a8067d16621d31c4dca75b0150930f30f0fc96
jxixeun <jxixeun@gmail.com>
2023-11-06 13:34:54 +0900

    :sparkles: Feat: JWT의 payload값을 디코딩할 TokenManager 구현

---
+++ groo/src/main/java/com/eokam/groo/infrastructure/jwt/TokenManager.java
@@ -0,0 +1,21 @@
+package com.eokam.groo.infrastructure.jwt;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+@Component
+public class TokenManager {
+
+	public Claims getTokenClaims(String token) {
+		String[] splitToken = token.split("\\.");
+		String unsignedToken = splitToken[0] + "." + splitToken[1] + ".";
+		return Jwts.parser().parseClaimsJwt(unsignedToken).getBody();
+	}
+
+	public Long getMemberId(String token){
+		Claims tokenClaims = getTokenClaims(token);
+		return tokenClaims.get("memberId", Long.class);
+	}
+}
