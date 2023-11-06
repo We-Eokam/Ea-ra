4b3b7ebb84ac58539df82ff9cbefa995c6d1a095
jxixeun <jxixeun@gmail.com>
2023-11-06 13:33:33 +0900

    :sparkles: Feat: GrooSavingDto 구현

---
+++ groo/src/main/java/com/eokam/groo/application/dto/GrooSavingDto.java
@@ -0,0 +1,38 @@
+package com.eokam.groo.application.dto;
+
+import java.time.LocalDateTime;
+
+import com.eokam.groo.domain.entity.GrooSaving;
+import com.eokam.groo.global.constant.ActivityType;
+import com.eokam.groo.global.constant.SavingType;
+import com.eokam.groo.presentation.dto.GrooSavingRequest;
+
+import lombok.Builder;
+
+@Builder
+public record GrooSavingDto(Long savingId, Long memberId, Long amount, Long remainGroo, Long proofAccusationId, SavingType savingType, ActivityType activityType, LocalDateTime savedAt) {
+
+	public static GrooSavingDto from(GrooSaving grooSaving) {
+		return GrooSavingDto.builder()
+			.savingId(grooSaving.getSavingId())
+			.memberId(grooSaving.getMemberId())
+			.amount(grooSaving.getAmount())
+			.savingType(grooSaving.getSavingType())
+			.proofAccusationId(grooSaving.getProofAccusationId())
+			.activityType(grooSaving.getActivityType())
+			.remainGroo(grooSaving.getRemainGroo())
+			.savedAt(grooSaving.getSavedAt())
+			.build();
+	}
+
+	public static GrooSavingDto of(GrooSavingRequest request, Long memberId) {
+		System.out.println(request.getActivityType());
+		return GrooSavingDto.builder()
+			.memberId(memberId)
+			.savingType(request.getSavingType())
+			.activityType(request.getActivityType())
+			.proofAccusationId(request.getProofAccusationId())
+			.savedAt(request.getSavedAt())
+			.build();
+	}
+}
