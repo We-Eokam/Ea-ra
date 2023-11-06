3b776929e0676dd0c583fcdbc88bc07444312214
jxixeun <jxixeun@gmail.com>
2023-11-06 17:56:23 +0900

    :sparkles: Feat: 특정 월의 그린 적립 내역 조회 API 구현

--- groo/src/main/java/com/eokam/groo/presentation/controller/GrooSavingController.java
+++ groo/src/main/java/com/eokam/groo/presentation/controller/GrooSavingController.java
@@ -4,11 +4,14 @@ import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eokam.groo.application.dto.GrooMonthDto;
 import com.eokam.groo.application.dto.GrooSavingDto;
 import com.eokam.groo.application.service.GrooSavingService;
 import com.eokam.groo.infrastructure.jwt.TokenManager;
@@ -31,5 +34,12 @@ public class GrooSavingController {
 		GrooSavingDto grooSavingDto = grooSavingService.createGrooSaving(GrooSavingDto.of(request, memberId));
 		ResponseEntity.created(URI.create("/groo/" + grooSavingDto.savingId())).body(GrooSavingResponse.from(grooSavingDto));
 		return ResponseEntity.created(URI.create("/groo/" + grooSavingDto.savingId())).body(GrooSavingResponse.from(grooSavingDto));
+	}
+
+	@GetMapping
+	ResponseEntity<?> getDailySavingAmountByMonth(@CookieValue(value = "access-token") String jwt, @RequestParam Integer year, @RequestParam Integer month) {
+		Long memberId = tokenManager.getMemberId(jwt);
+		GrooMonthDto dailySavingAmountByMonth = grooSavingService.getDailySavingAmountByMonth(memberId, year, month);
+		return ResponseEntity.ok().body(dailySavingAmountByMonth);
 	}
 }
