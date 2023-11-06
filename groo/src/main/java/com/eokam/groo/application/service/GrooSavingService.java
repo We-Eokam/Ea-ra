5741aab93e124e4ea84c2c6e215d11d567ef13dd
jxixeun <jxixeun@gmail.com>
2023-11-06 13:31:50 +0900

    :sparkles: Feat: 그루 적립 서비스 인터페이스 생성 및 메서드 선언

---
+++ groo/src/main/java/com/eokam/groo/application/service/GrooSavingService.java
@@ -0,0 +1,8 @@
+package com.eokam.groo.application.service;
+
+import com.eokam.groo.application.dto.GrooSavingDto;
+
+public interface GrooSavingService {
+
+	GrooSavingDto createGrooSaving(GrooSavingDto grooSavingDto);
+}
