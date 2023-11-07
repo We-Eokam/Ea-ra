414c673b7e3fb6784faaba673f173d7491c752b6
이상재 <tkdwo7699@gmail.com>
2023-11-07 11:12:29 +0900

    ✅Test: 카타고리화된 매장정보 조회 서비스 테스트 생성

--- cpoint/src/test/java/com/eokam/cpoint/application/service/StoreServiceTest.java
+++ cpoint/src/test/java/com/eokam/cpoint/application/service/StoreServiceTest.java
@@ -11,9 +11,9 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 
 import com.eokam.cpoint.application.common.BaseServiceTest;
+import com.eokam.cpoint.application.dto.ActivityStroeClassDto;
 import com.eokam.cpoint.application.dto.StoreClassDto;
 import com.eokam.cpoint.application.dto.StoreDto;
 import com.eokam.cpoint.domain.ActivityType;
@@ -122,6 +122,60 @@ public class StoreServiceTest extends BaseServiceTest {
 					assertThat(coordinateLng).isNotNull();
 					assertThat(coordinateLng).isGreaterThanOrEqualTo(0);
 				});
+		}
+
+		@Test
+		void 모든활동타입을_사용하여_활동타입으로나눠_조회() {
+			//given
+			Integer radius = 100;
+			Double longitude = 127.030454;
+			Double latitude = 37.510223;
+
+			Company 스타벅스_텀블러 = Company.builder().id(1L).name("스타벅스").build();
+			Company 메가커피_텀블러 = Company.builder().id(2L).name("메가커피").build();
+
+			Company CU_전자영수증 = Company.builder().id(3L).name("CU").build();
+			Company 이마트_전자영수증 = Company.builder().id(4L).name("이마트").build();
+
+			given(companyRepository.findById(1L)).willReturn(Optional.of(스타벅스_텀블러));
+			given(companyRepository.findById(2L)).willReturn(Optional.of(메가커피_텀블러));
+			given(companyRepository.findById(3L)).willReturn(Optional.of(CU_전자영수증));
+			given(companyRepository.findById(4L)).willReturn(Optional.of(이마트_전자영수증));
+
+			List<StoreDto> 텀블러_매장 = 회사PK가_1인스타벅스_2인메가커피_각매장2개생성();
+			List<StoreDto> 전자영수증_매장 = 회사PK가_3인이마트_4인CU_각매장2개생성();
+
+			given(storeRepository.getStoresListByCurrentPositionAndRadiusAndActivityType(latitude, longitude, radius,
+				ActivityType.TUMBLER)).willReturn(텀블러_매장);
+
+			given(storeRepository.getStoresListByCurrentPositionAndRadiusAndActivityType(latitude, longitude, radius,
+				ActivityType.ELECTRONIC_RECEIPT)).willReturn(전자영수증_매장);
+
+			//when
+			List<ActivityStroeClassDto> 조회결과 =
+				storeService.retrieveNearCpointStoreCategorizedByActivityType(radius, latitude, longitude);
+
+			//then
+			assertThat(조회결과).hasSize(2);
+			assertThat(조회결과)
+				.filteredOn(dto -> dto.getActivityType().equals(ActivityType.TUMBLER))
+				.allSatisfy(dto -> assertThat(dto.getStores()).hasSize(4));
+
+			assertThat(조회결과)
+				.filteredOn(dto -> dto.getActivityType().equals(ActivityType.TUMBLER))
+				.flatExtracting(ActivityStroeClassDto::getStores)
+				.extracting(StoreClassDto::getCompanyId, StoreClassDto::getCompanyName)
+				.containsOnly(tuple(1L, "스타벅스"), tuple(2L, "메가커피"));
+
+			assertThat(조회결과)
+				.filteredOn(dto -> dto.getActivityType().equals(ActivityType.ELECTRONIC_RECEIPT))
+				.allSatisfy(dto -> assertThat(dto.getStores()).hasSize(4));
+
+			assertThat(조회결과)
+				.filteredOn(dto -> dto.getActivityType().equals(ActivityType.ELECTRONIC_RECEIPT))
+				.flatExtracting(ActivityStroeClassDto::getStores)
+				.extracting(StoreClassDto::getCompanyId, StoreClassDto::getCompanyName)
+				.containsOnly(tuple(3L, "CU"), tuple(4L, "이마트"));
 		}
 
 		List<StoreDto> 회사PK가_1인스타벅스_2인메가커피_각매장2개생성() {
@@ -239,6 +293,125 @@ public class StoreServiceTest extends BaseServiceTest {
 			storeDtoList.add(스타벅스_역삼대로점);
 			storeDtoList.add(메가커피_강남역점);
 			storeDtoList.add(메가커피_언주로점);
+
+			return storeDtoList;
+		}
+
+		List<StoreDto> 회사PK가_3인이마트_4인CU_각매장2개생성() {
+			List<StoreDto> storeDtoList = new ArrayList<>();
+
+			StoreDto 이마트_역삼점 = new StoreDto() {
+				@Override
+				public Long getCompanyId() {
+					return 3L;
+				}
+
+				@Override
+				public String getBranch() {
+					return "역삼점";
+				}
+
+				@Override
+				public Double getDistance() {
+					return 500.0;
+				}
+
+				@Override
+				public Double getLatitude() {
+					return 37.497957;
+				}
+
+				@Override
+				public Double getLongitude() {
+					return 127.026063;
+				}
+			};
+
+			StoreDto 이마트_가양점 = new StoreDto() {
+				@Override
+				public Long getCompanyId() {
+					return 3L;
+				}
+
+				@Override
+				public String getBranch() {
+					return "가양점";
+				}
+
+				@Override
+				public Double getDistance() {
+					return 150.0;
+				}
+
+				@Override
+				public Double getLatitude() {
+					return 37.501599;
+				}
+
+				@Override
+				public Double getLongitude() {
+					return 127.040338;
+				}
+			};
+
+			StoreDto CU_강남역점 = new StoreDto() {
+				@Override
+				public Long getCompanyId() {
+					return 4L;
+				}
+
+				@Override
+				public String getBranch() {
+					return "강남역점";
+				}
+
+				@Override
+				public Double getDistance() {
+					return 500.0;
+				}
+
+				@Override
+				public Double getLatitude() {
+					return 37.497966;
+				}
+
+				@Override
+				public Double getLongitude() {
+					return 127.027369;
+				}
+			};
+
+			StoreDto CU_신목동점 = new StoreDto() {
+				@Override
+				public Long getCompanyId() {
+					return 4L;
+				}
+
+				@Override
+				public String getBranch() {
+					return "신목동점";
+				}
+
+				@Override
+				public Double getDistance() {
+					return 150.0;
+				}
+
+				@Override
+				public Double getLatitude() {
+					return 37.497966;
+				}
+
+				@Override
+				public Double getLongitude() {
+					return 127.027369;
+				}
+			};
+
+			storeDtoList.add(이마트_가양점);
+			storeDtoList.add(이마트_역삼점);
+			storeDtoList.add(CU_강남역점);
+			storeDtoList.add(CU_신목동점);
 
 			return storeDtoList;
 		}
