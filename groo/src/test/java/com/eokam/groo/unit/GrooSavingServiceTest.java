416dadfbfd5ba970a77fecc4f3d809e8299c4f9f
jxixeun <jxixeun@gmail.com>
2023-11-07 15:58:06 +0900

    :art: Refactor: 사용하지 않는 import문 삭제

--- groo/src/test/java/com/eokam/groo/unit/GrooSavingServiceTest.java
+++ groo/src/test/java/com/eokam/groo/unit/GrooSavingServiceTest.java
@@ -16,7 +16,6 @@ import java.util.Random;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
