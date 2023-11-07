d231fa559e2f7e27d7ad183c64f3f17fed0b738d
jxixeun <jxixeun@gmail.com>
2023-11-07 11:27:30 +0900

    :white_check_mark: Test: 특정 월의 그린 적립 내역 조회 서비스 단위 테스트 작성

--- groo/src/test/java/com/eokam/groo/unit/GrooSavingServiceTest.java
+++ groo/src/test/java/com/eokam/groo/unit/GrooSavingServiceTest.java
@@ -1,25 +1,32 @@
 package com.eokam.groo.unit;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.eokam.groo.application.dto.GrooMonthDto;
 import com.eokam.groo.application.dto.GrooSavingDto;
-import com.eokam.groo.application.service.GrooSavingService;
 import com.eokam.groo.application.service.GrooSavingServiceImpl;
 import com.eokam.groo.domain.entity.GrooSaving;
 import com.eokam.groo.global.constant.ActivityType;
 import com.eokam.groo.global.constant.SavingType;
+import com.eokam.groo.infrastructure.dto.GrooDailySumAmountDto;
+import com.eokam.groo.infrastructure.dto.GrooMonthSumAmountDto;
 import com.eokam.groo.infrastructure.repository.GrooSavingRepository;
 
 @ActiveProfiles("test")
@@ -35,6 +42,11 @@ public class GrooSavingServiceTest {
 	public void setUp() throws Exception {
 		MockitoAnnotations.openMocks(this);
 	}
+
+	private static Long expectedAccusationSum;
+	private static Long expectedAccusationCount;
+	private static Long expectedProofSum;
+	private static Long expectedProofCount;
 
 	@Test
 	@DisplayName("그루 적립 내역을 저장한다.")
@@ -59,13 +71,63 @@ public class GrooSavingServiceTest {
 			.proofAccusationId(2L)
 			.remainGroo(100L)
 			.build();
-		BDDMockito.given(grooSavingRepository.save(any())).willReturn(grooSaving);
+		given(grooSavingRepository.save(any())).willReturn(grooSaving);
 
 		// when
 		GrooSavingDto getGrooSavingDto = grooSavingService.createGrooSaving(grooSavingDto);
 
 		// then
-		BDDMockito.verify(grooSavingRepository).save(any());
-		Assertions.assertThat(getGrooSavingDto.savedAt()).isEqualTo(grooSavingDto.savedAt());
+		verify(grooSavingRepository).save(any());
+		assertThat(getGrooSavingDto.savedAt()).isEqualTo(grooSavingDto.savedAt());
 	}
+
+	@Test
+	@DisplayName("특정 월의 그린 적립 내역을 조회할 수 있다.")
+	void getDailySavingAmountByMonth() {
+		//given
+		List<GrooDailySumAmountDto> grooDailySumAmountDtoList = getGrooDailySumAmountDtoList();
+		GrooMonthSumAmountDto grooMonthSumAmountDto = new GrooMonthSumAmountDto(expectedProofSum, expectedProofCount,
+			expectedAccusationSum, expectedAccusationCount);
+		given(grooSavingRepository.getDailySumAndAmount(1L, 2023, 11)).willReturn(grooDailySumAmountDtoList);
+		given(grooSavingRepository.getSumAndAmountByMonth(1L, 2023, 11)).willReturn(grooMonthSumAmountDto);
+
+		//then
+		GrooMonthDto grooMonthDto = grooSavingService.getDailySavingAmountByMonth(1L, 2023, 11);
+
+		//
+		verify(grooSavingRepository).getDailySumAndAmount(1L,2023,11);
+		verify(grooSavingRepository).getSumAndAmountByMonth(1L, 2023, 11);
+		assertThat(grooMonthDto.accusationCount()).isEqualTo(grooMonthSumAmountDto.getAccusationCount());
+		assertThat(grooMonthDto.accusationSum()).isEqualTo(grooMonthSumAmountDto.getAccusationSum());
+		assertThat(grooMonthDto.proofCount()).isEqualTo(grooMonthSumAmountDto.getProofCount());
+		assertThat(grooMonthDto.proofSum()).isEqualTo(grooMonthSumAmountDto.getProofSum());
+		assertThat(grooMonthDto.grooSavingList().size()).isEqualTo(grooDailySumAmountDtoList.size());
+	}
+
+	public List<GrooDailySumAmountDto> getGrooDailySumAmountDtoList() {
+		expectedAccusationSum = 0L;
+		expectedAccusationCount = 0L;
+		expectedProofSum = 0L;
+		expectedProofCount = 0L;
+
+		List<GrooDailySumAmountDto> grooDailySumAmountDtos = new ArrayList<>();
+		for (int i =0;i<30;i++){
+			int randomNum = new Random().nextInt(50);
+			long sum1 = new Random().nextLong(1000L);
+			long sum2 = new Random().nextLong(1000L);
+			long count1 = new Random().nextLong(10L);
+			long count2 = new Random().nextLong(10L);
+			LocalDate localDate = LocalDate.now().plusDays(randomNum);
+			Date date = Date.valueOf(String.valueOf(localDate));
+			grooDailySumAmountDtos.add(new GrooDailySumAmountDto(date, sum1, count1, sum2, count2));
+			if (localDate.getMonth().getValue() == 11) {
+				expectedAccusationSum += sum2;
+				expectedAccusationCount += count2;
+				expectedProofSum += sum1;
+				expectedProofCount += count1;
+			}
+		}
+		return grooDailySumAmountDtos;
+	}
+
 }
