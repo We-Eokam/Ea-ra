00450cbe92c33bf2fd4ca6cd7de01cef02e91a1a
이상재 <tkdwo7699@gmail.com>
2023-11-08 10:54:10 +0900

    ✨Feat: 탄소중립포인트 request response dto 생성

---
+++ cpoint/src/main/java/com/eokam/cpoint/presentation/dto/CpointResponse.java
@@ -0,0 +1,18 @@
+package com.eokam.cpoint.presentation.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class CpointResponse {
+
+	private Long memberId;
+
+	private Integer cpoint;
+
+}
