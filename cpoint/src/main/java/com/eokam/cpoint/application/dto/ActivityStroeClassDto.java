46eeb7c12821e41f982202978475ddafe0c9f5a7
이상재 <tkdwo7699@gmail.com>
2023-11-08 16:09:44 +0900

    ✨Feat: 매장 서비스 dto json 매핑 설정 추가

--- cpoint/src/main/java/com/eokam/cpoint/application/dto/ActivityStroeClassDto.java
+++ cpoint/src/main/java/com/eokam/cpoint/application/dto/ActivityStroeClassDto.java
@@ -3,12 +3,15 @@ package com.eokam.cpoint.application.dto;
 import java.util.List;
 
 import com.eokam.cpoint.domain.ActivityType;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ActivityStroeClassDto {
 
 	private ActivityType activityType;
