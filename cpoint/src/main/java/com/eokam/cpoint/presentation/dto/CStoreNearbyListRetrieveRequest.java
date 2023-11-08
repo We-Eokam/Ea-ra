c487a1766f0705d6da2588c434703474bd14133f
이상재 <tkdwo7699@gmail.com>
2023-11-08 14:55:24 +0900

    🎨Refactor: 매장조회 request dto 필드 오타 수정

--- cpoint/src/main/java/com/eokam/cpoint/presentation/dto/CStoreNearbyListRetrieveRequest.java
+++ cpoint/src/main/java/com/eokam/cpoint/presentation/dto/CStoreNearbyListRetrieveRequest.java
@@ -1,5 +1,6 @@
 package com.eokam.cpoint.presentation.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,9 +13,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CStoreNearbyListRetrieveRequest {
 
+	@NotNull(message = "latitude가 필요합니다.")
 	Double latitude;
 
+	@NotNull(message = "longitude가 필요합니다.")
 	Double longitude;
 
+	@NotNull(message = "radius가 필요합니다.")
 	Integer radius;
 }
