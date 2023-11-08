84b1b06055acd61d5be300d7ab922e413786907c
이상재 <tkdwo7699@gmail.com>
2023-11-08 10:58:52 +0900

    ✅Test: 컨트롤러 공통 테스트 추가

---
+++ cpoint/src/test/java/com/eokam/cpoint/presentation/common/BaseControllerTest.java
@@ -0,0 +1,49 @@
+package com.eokam.cpoint.presentation.common;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import com.eokam.cpoint.config.RestDocsConfig;
+import com.eokam.cpoint.global.WebConfig;
+import com.eokam.cpoint.presentation.controller.CompanyController;
+import com.eokam.cpoint.presentation.controller.CpointController;
+import com.eokam.cpoint.presentation.controller.StoreControllerTest;
+
+@Disabled
+@AutoConfigureRestDocs
+@Import({RestDocsConfig.class,WebConfig.class})
+@ExtendWith(RestDocumentationExtension.class)
+@WebMvcTest({CompanyController.class})
+public class BaseControllerTest {
+
+	protected static String 멤버Id가10인_JWT쿠키 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZW1iZXJJZCI6MTB9.mmEC4b0afZan9cC90acvqCzaLNzQboEkJctrs44W6oI";
+	@Autowired
+	protected MockMvc mockMvc;
+	@Autowired
+	protected RestDocumentationResultHandler restDocs;
+
+	@BeforeEach
+	void setUp(final WebApplicationContext context,
+		final RestDocumentationContextProvider provider) {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+			.apply(MockMvcRestDocumentation.documentationConfiguration(provider))  // rest docs 설정 주입
+			.alwaysDo(MockMvcResultHandlers.print()) // andDo(print()) 코드 포함
+			.alwaysDo(restDocs) // pretty 패턴과 문서 디렉토리 명 정해준것 적용
+			.addFilters(new CharacterEncodingFilter("UTF-8", true)) // 한글 깨짐 방지
+			.build();
+	}
+}
