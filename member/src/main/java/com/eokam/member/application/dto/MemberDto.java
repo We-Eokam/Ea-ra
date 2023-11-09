cc8bf6f925416b66df449c3e2af3c06e5e8fd902
이상재 <tkdwo7699@gmail.com>
2023-11-09 14:29:09 +0900

    ✨Feat: 회원DTO 생성

---
+++ member/src/main/java/com/eokam/member/application/dto/MemberDto.java
@@ -0,0 +1,35 @@
+package com.eokam.member.application.dto;
+
+import com.eokam.member.domain.Member;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class MemberDto {
+
+	private Long memberId;
+
+	private String nickname;
+
+	private Integer groo;
+
+	private Integer bill;
+
+	private Integer billCount;
+
+	private String profileImage;
+
+	public static MemberDto from(Member member){
+		return MemberDto
+			.builder()
+			.memberId(member.getId())
+			.nickname(member.getNickname())
+			.groo(member.getGroo())
+			.bill(member.getBill())
+			.billCount(member.getBillCount())
+			.profileImage(member.getProfileImage())
+			.build();
+	}
+}
