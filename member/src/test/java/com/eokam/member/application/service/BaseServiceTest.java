b5b7794460bffb7ed9d1c860d3af21df65e853b4
이상재 <tkdwo7699@gmail.com>
2023-11-09 14:36:47 +0900

    ✅Test: 공용 서비스 테스트 생성

---
+++ member/src/test/java/com/eokam/member/application/service/BaseServiceTest.java
@@ -0,0 +1,20 @@
+package com.eokam.member.application.service;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+import jakarta.transaction.Transactional;
+
+@Transactional
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class BaseServiceTest {
+
+}
+
+
