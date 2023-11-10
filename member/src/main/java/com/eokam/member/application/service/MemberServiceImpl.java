c2bbbc66f156c525974231617252dccce68935fe
이상재 <tkdwo7699@gmail.com>
2023-11-10 09:45:30 +0900

    ✨Feat: 멤버 정보 관련 서비스 구현

--- member/src/main/java/com/eokam/member/application/service/MemberServiceImpl.java
+++ member/src/main/java/com/eokam/member/application/service/MemberServiceImpl.java
@@ -4,12 +4,18 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.eokam.member.application.dto.MemberDto;
 import com.eokam.member.domain.Member;
+import com.eokam.member.domain.SavingType;
 import com.eokam.member.global.ErrorCode;
 import com.eokam.member.global.exception.MemberNotFoundException;
+import com.eokam.member.global.exception.NicknameAlreadyExistException;
+import com.eokam.member.global.exception.NoBillException;
 import com.eokam.member.infra.dto.JwtMemberDto;
+import com.eokam.member.infra.external.S3FileDetail;
+import com.eokam.member.infra.external.service.S3Service;
 import com.eokam.member.infra.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -20,39 +26,85 @@ public class MemberServiceImpl implements MemberService {
 
 	private final MemberRepository memberRepository;
 
+	private final S3Service s3Service;
+
 	@Override
 	@Transactional(readOnly = true)
 	public List<MemberDto> retrieveMemberProfile(List<Long> memberIdList) {
-		return null;
+		List<Member> memberList = memberRepository.findAllById(memberIdList);
+		if(memberList.size() != memberIdList.size()){
+			throw new MemberNotFoundException(ErrorCode.MEMBER_NOT_FOUND);
+		}
+		return memberList.stream().map(member->MemberDto.from(member)).toList();
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public MemberDto retrieveMemberInfo(JwtMemberDto memberDto) {
-		return null;
+		return MemberDto.from(memberRepository.findById(memberDto.getMemberId())
+			.orElseThrow(()-> new MemberNotFoundException(ErrorCode.MEMBER_NOT_FOUND)));
 	}
 
 	@Override
-	public MemberDto updateMemberProfile(MemberDto memberDto) {
-		return null;
+	@Transactional
+	public MemberDto repayGroo(Long memberId, SavingType savingType, Integer groo) {
+		Member member = memberRepository
+			.findById(memberId).orElseThrow(()->new MemberNotFoundException(ErrorCode.MEMBER_NOT_FOUND));
+
+		member.repayGroo(savingType,groo);
+
+		return MemberDto.from(member);
 	}
 
 	@Override
-	public MemberDto repayGroo(Integer groo) {
-		return null;
+	@Transactional
+	public Boolean addBillCount(Long memberId) {
+		Member member = memberRepository
+			.findById(memberId).orElseThrow(()->new MemberNotFoundException(ErrorCode.MEMBER_NOT_FOUND));
+
+		return member.addBillCount();
 	}
 
 	@Override
-	public void addBillCount() {
+	@Transactional
+	public Boolean useBill(Long memberId) {
+		Member member = memberRepository
+			.findById(memberId).orElseThrow(()-> new MemberNotFoundException(ErrorCode.MEMBER_NOT_FOUND));
 
+		if(!member.useBill()){
+			throw new NoBillException(ErrorCode.BILL_NOT_ENOUGH);
+		}
+		return true;
 	}
 
 	@Override
-	public void useBill() {
-
-	}
-
-	@Override
+	@Transactional(readOnly = true)
 	public Boolean checkDuplicateNickname(String nickname) {
-		return null;
+		if(!memberRepository.findMemberByNickname(nickname).isEmpty()){
+			throw new NicknameAlreadyExistException(ErrorCode.NICKNAME_ALREADY_EXIST);
+		}
+		return true;
 	}
+
+	@Override
+	@Transactional
+	public MemberDto updateNickname(JwtMemberDto jwtMemberDto, String nickname) {
+		Member member = memberRepository
+			.findById(jwtMemberDto.getMemberId()).orElseThrow(()-> new MemberNotFoundException(ErrorCode.MEMBER_NOT_FOUND));
+
+		member.changeNickname(nickname);
+		return MemberDto.from(member);
+	}
+
+	@Override
+	@Transactional
+	public MemberDto updateProfileImage(JwtMemberDto jwtMemberDto, MultipartFile multipartFile) {
+		Member member = memberRepository
+			.findById(jwtMemberDto.getMemberId()).orElseThrow(()-> new MemberNotFoundException(ErrorCode.MEMBER_NOT_FOUND));
+		S3FileDetail s3FileDetail = s3Service.save(multipartFile);
+		member.changeProfileImage(s3FileDetail.getFileName(),s3FileDetail.getUrl());
+		return MemberDto.from(member);
+	}
+
+
 }
