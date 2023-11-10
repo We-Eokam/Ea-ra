1f2ecf2884c95e8809dc7dfe0ad7dd07567ce372
이상재 <tkdwo7699@gmail.com>
2023-11-10 09:41:54 +0900

    ✨Feat: 전역 에러 코드 닉네임 s3 고발장 추가

--- member/src/main/java/com/eokam/member/global/ErrorCode.java
+++ member/src/main/java/com/eokam/member/global/ErrorCode.java
@@ -6,7 +6,10 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND,"MEM_001","해당하는 멤버가 없읍니다");
+	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND,"MEM_001","해당하는 멤버가 없읍니다."),
+	BILL_NOT_ENOUGH(HttpStatus.BAD_REQUEST,"BILL_001","고소장이 없읍니다."),
+	S3_NOT_UPLOAD(HttpStatus.INTERNAL_SERVER_ERROR,"S3_001","S3에 파일 업로드가 실패합니다."),
+	NICKNAME_ALREADY_EXIST(HttpStatus.BAD_REQUEST,"NICKNAME_001","이미 존재하는 닉네임입니다.");
 	ErrorCode(HttpStatus httpStatus, String errorCode, String message) {
 		this.httpStatus = httpStatus;
 		this.errorCode = errorCode;
