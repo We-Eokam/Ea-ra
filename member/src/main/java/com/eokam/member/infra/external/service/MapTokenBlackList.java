b9f0b9287d68ed6f86ae574c79627cc604fab66b
이상재 <tkdwo7699@gmail.com>
2023-11-10 16:49:55 +0900

    ✨Feat: 액세스 토큰 블랙리스트 구현

---
+++ member/src/main/java/com/eokam/member/infra/external/service/MapTokenBlackList.java
@@ -0,0 +1,25 @@
+package com.eokam.member.infra.external.service;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.stereotype.Component;
+
+import com.eokam.member.global.ErrorCode;
+import com.eokam.member.global.exception.LoginException;
+
+@Component
+public class MapTokenBlackList implements TokenBlackList{
+
+	private final ConcurrentHashMap<String,Boolean> blackList = new ConcurrentHashMap<>();
+
+	@Override
+	public void invalidate(String accessToken) {
+		if(!blackList.containsKey(accessToken)) throw new LoginException(ErrorCode.TOKEN_ALREADY_LOGOUT);
+		blackList.remove(accessToken);
+	}
+
+	@Override
+	public Boolean validate(String accessToken) {
+		return !blackList.containsKey(accessToken);
+	}
+}
