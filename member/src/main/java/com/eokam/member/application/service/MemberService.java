47f42fed1a28a6614f73147994a051340080fe9c
이상재 <tkdwo7699@gmail.com>
2023-11-10 09:45:10 +0900

    ✨Feat: 멤버 서비스 프로필 업데이트 메소드 추가

--- member/src/main/java/com/eokam/member/application/service/MemberService.java
+++ member/src/main/java/com/eokam/member/application/service/MemberService.java
@@ -2,7 +2,11 @@ package com.eokam.member.application.service;
 
 import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.eokam.member.application.dto.MemberDto;
+import com.eokam.member.domain.Member;
+import com.eokam.member.domain.SavingType;
 import com.eokam.member.infra.dto.JwtMemberDto;
 
 public interface MemberService {
@@ -11,13 +15,15 @@ public interface MemberService {
 
 	public MemberDto retrieveMemberInfo(JwtMemberDto memberDto);
 
-	public MemberDto updateMemberProfile(MemberDto memberDto);
+	public MemberDto repayGroo(Long memberId, SavingType savingType, Integer groo);
 
-	public MemberDto repayGroo(Integer groo);
+	public Boolean addBillCount(Long memberId);
 
-	public void addBillCount();
+	public Boolean useBill(Long memberId);
 
-	public void useBill();
+	public MemberDto updateNickname(JwtMemberDto jwtMemberDto, String nickname);
+
+	public MemberDto updateProfileImage(JwtMemberDto jwtMemberDto, MultipartFile multipartFile);
 
 	public Boolean checkDuplicateNickname(String nickname);
 }
