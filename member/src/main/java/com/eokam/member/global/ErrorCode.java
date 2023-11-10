e81dd571ea660c7de40e609457541138dc1f942b
이상재 <tkdwo7699@gmail.com>
2023-11-10 16:41:41 +0900

    ✨Feat: 로그인 관련 에러 코드 추가

--- member/src/main/java/com/eokam/member/global/ErrorCode.java
+++ member/src/main/java/com/eokam/member/global/ErrorCode.java
@@ -9,7 +9,13 @@ public enum ErrorCode {
 	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND,"MEM_001","해당하는 멤버가 없읍니다."),
 	BILL_NOT_ENOUGH(HttpStatus.BAD_REQUEST,"BILL_001","고소장이 없읍니다."),
 	S3_NOT_UPLOAD(HttpStatus.INTERNAL_SERVER_ERROR,"S3_001","S3에 파일 업로드가 실패합니다."),
-	NICKNAME_ALREADY_EXIST(HttpStatus.BAD_REQUEST,"NICKNAME_001","이미 존재하는 닉네임입니다.");
+	NICKNAME_ALREADY_EXIST(HttpStatus.BAD_REQUEST,"NICKNAME_001","이미 존재하는 닉네임입니다."),
+	TOKEN_ALREADY_LOGOUT(HttpStatus.UNAUTHORIZED,"LOGIN_001","이미 로그아웃된 토큰입니다."),
+	KAKAO_TOKEN_REQUEST_FAIL(HttpStatus.INTERNAL_SERVER_ERROR,"LOGIN_002","카카오 access token 요청이 실패했습니다."),
+	KAKAO_SERVER_ERROR(HttpStatus.BAD_REQUEST,"LOGIN_003","카카오 서버의 문제로 로그인에 실패했습니다"),
+	KAKAO_MEMBER_REQUEST_FAIL(HttpStatus.INTERNAL_SERVER_ERROR,"LOGIN_004","카카오 정보 요청이 실패했습니다."),
+	JWT_PARSE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR,"JWT_001","JWT 토큰 파싱 과정에서 오류가 발생했습니다."),
+	INVALID_JWT_TOKEN(HttpStatus.UNAUTHORIZED,"JWT_002","유효하지 않은 JWT 토큰입니다.");
 	ErrorCode(HttpStatus httpStatus, String errorCode, String message) {
 		this.httpStatus = httpStatus;
 		this.errorCode = errorCode;
