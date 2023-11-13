c3aff6a7de924d6da4eb9cbf3999c9e409e197af
jxixeun <jxixeun@gmail.com>
2023-11-13 18:11:49 +0900

    :sparkles: Feat: ErrorCode 추가

--- accusation/src/main/java/com/eokam/accusation/global/error/ErrorCode.java
+++ accusation/src/main/java/com/eokam/accusation/global/error/ErrorCode.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-	ACCUSATION_NOT_EXIST(HttpStatus.NOT_FOUND, "AC-001", "요청한 고발장이 존재하지 않습니다.");
+	ACCUSATION_NOT_EXIST(HttpStatus.NOT_FOUND, "AC-001", "요청한 고발장이 존재하지 않습니다."),
+	READ_PERMISSION_DENIED(HttpStatus.UNAUTHORIZED, "AC-002", "읽기 권한이 없습니다."),
+	SELF_ACCUSATION_RESTRICTED(HttpStatus.UNAUTHORIZED, "AC-003", "자신이 자신에게 제보할 수 없습니다.");
 
 	ErrorCode(HttpStatus httpStatus, String errorCode, String message) {
 		this.httpStatus = httpStatus;
