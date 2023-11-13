ac6e194472ba7e78fb50f2074f5a523e76065a17
이상재 <tkdwo7699@gmail.com>
2023-11-14 06:27:04 +0900

    ✨Feat: cors 설정 추가

--- member/src/main/java/com/eokam/member/global/WebConfig.java
+++ member/src/main/java/com/eokam/member/global/WebConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.RequiredArgsConstructor;
@@ -17,5 +18,10 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(memberArgumentResolver);
+	}
+
+	@Override
+	public void addCorsMappings(final CorsRegistry registry) {
+		registry.addMapping("/**");
 	}
 }
