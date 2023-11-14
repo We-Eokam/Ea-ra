b8d08a2df44712f07c3f2c053b96a4b20ef24750
이상재 <tkdwo7699@gmail.com>
2023-11-14 14:48:41 +0900

    Update member/src/main/java/com/eokam/member/presentation/controller/LoginController.java

--- member/src/main/java/com/eokam/member/presentation/controller/LoginController.java
+++ member/src/main/java/com/eokam/member/presentation/controller/LoginController.java
@@ -46,6 +46,7 @@ public class LoginController {
 	public ResponseEntity<?> loginDone(@RequestParam(value = "code", required = false) String authorizeCode){
 		String accessToken = loginService.login(authorizeCode);
 		ResponseCookie responseCookie = ResponseCookie.from("access-token",accessToken)
+		    .samesite("None")
 			.httpOnly(true)
 			.secure(true)
 			.path("/")
