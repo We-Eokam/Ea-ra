d663cfa77cd79c6d19187fea0f71978b2b954ad1
이상재 <tkdwo7699@gmail.com>
2023-11-14 23:22:24 +0900

    Refactor: 오타 수정

--- cpoint/src/main/java/com/eokam/cpoint/infra/MysqlStoreRepository.java
+++ cpoint/src/main/java/com/eokam/cpoint/infra/MysqlStoreRepository.java
@@ -27,7 +27,7 @@ public interface MysqlStoreRepository extends StoreRepository {
 		+ " s.latitude as latitude,"
 		+ " s.longitude as longitude"
 		+ " FROM store as s"
-		+ " INNER JOIN company_poilicy as cp"
+		+ " INNER JOIN company_policy as cp"
 		+ " ON cp.company_id = s.company_id"
 		+ " AND cp.activity_type = :#{#activityType.name()}"
 		+ " WHERE ST_Distance_Sphere(POINT(:longitude,:latitude), POINT(s.longitude, s.latitude)) <= :radius"
