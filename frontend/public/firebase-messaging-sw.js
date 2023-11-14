cf0602541ccb1418c5cb93793404287e2f771db5
Jeong-Hoon-Yang <yjhoon3658@naver.com>
2023-11-14 23:05:49 +0900

    :sparkles: Feat: fcm service-worker 구현

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,25 +1,28 @@
-importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js");
+self.addEventListener("install", function (e) {
+  console.log("fcm sw install..");
+  self.skipWaiting();
+});
+self.addEventListener("activate", function (e) {
+  console.log("fcm sw activate..");
+});
+self.addEventListener("push", function (e) {
+  console.log("push: ", e.data.json());
+  if (!e.data.json()) return;
 
-const config = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.FIREBASE_APP_ID,
-  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
-};
-
-firebase.initializeApp(config);
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(messaging, (payload) => {
-  const notificationTitle = "Background Message Title";
+  const resultData = e.data.json().notification;
+  const notificationTitle = resultData.title;
   const notificationOptions = {
-    body: payload,
-    icon: "/firebase-logo.png",
+    body: resultData.body,
+    icon: resultData.image,
+    tag: resultData.tag,
+    ...resultData,
   };
-
+  console.log("push: ", { resultData, notificationTitle, notificationOptions });
   self.registration.showNotification(notificationTitle, notificationOptions);
+});
+self.addEventListener("notificationclick", function (event) {
+  console.log("notification click");
+  const url = "/";
+  event.notification.close();
+  event.waitUntil(clients.openWindow(url));
 });
--- frontend/src/util/fcm.js
+++ frontend/src/util/fcm.js
@@ -1,25 +1,25 @@
+import { getToken, onMessage, getMessaging } from "firebase/messaging";
 import { initializeApp } from "firebase/app";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 const config = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.FIREBASE_APP_ID,
-  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(config);
-const messaging = getMessaging();
+const messaging = getMessaging(app);
 
 getToken(messaging, {
-  vapidKey: import.meta.env.FIREBASE_VAPID_KEY,
+  vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY,
 })
   .then((currentToken) => {
     if (currentToken) {
-      localStorage.setItem(fcmToken, currentToken);
+      localStorage.setItem("FCM-key", currentToken);
     } else {
       console.log("No registration token available. Request permission to generate one.");
     }
