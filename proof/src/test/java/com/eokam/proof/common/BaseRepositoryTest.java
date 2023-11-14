9ece426728b0ef9392bc989a892a69a5fe55b5ec
Jeong-Hoon-Yang <yjhoon3658@naver.com>
2023-11-15 01:54:16 +0900

    :bug: Fix: Test 코드 프로파일 수정

--- proof/src/test/java/com/eokam/proof/common/BaseRepositoryTest.java
+++ proof/src/test/java/com/eokam/proof/common/BaseRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.eokam.proof.common;
 
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
+@ActiveProfiles("test")
 public class BaseRepositoryTest {
 }
