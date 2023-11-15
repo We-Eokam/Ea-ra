091526353f7194c5689f28fb36b3dbb30c8023d7
jxixeun <jxixeun@gmail.com>
2023-11-15 12:55:36 +0900

    :sparkles: Feat: Member Service에 그루 적립을 요청을 보내는 RebbitSender 구현

---
+++ groo/src/main/java/com/eokam/groo/infrastructure/rabbitmq/RabbitSender.java
@@ -0,0 +1,22 @@
+package com.eokam.groo.infrastructure.rabbitmq;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Configuration;
+
+import com.eokam.groo.infrastructure.dto.RepayGrooRequest;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class RabbitSender {
+	private final RabbitTemplate template;
+
+	public void send(RepayGrooRequest request) {
+		template.convertAndSend("repayGrooQueue", request);
+		log.info("Message {} sent successfully to queue repayGrooQueue", request);
+	}
+
+}
