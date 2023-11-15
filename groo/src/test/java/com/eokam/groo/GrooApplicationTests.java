821b78c4da923bc090a39828ebc1352dfc26a402
jxixeun <jxixeun@gmail.com>
2023-11-15 15:18:49 +0900

    :white_check_mark: Test: ActiveProfiles test로 변경

--- groo/src/test/java/com/eokam/groo/GrooApplicationTests.java
+++ groo/src/test/java/com/eokam/groo/GrooApplicationTests.java
@@ -3,8 +3,10 @@ package com.eokam.groo;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GrooApplicationTests {
 
 	@Test
