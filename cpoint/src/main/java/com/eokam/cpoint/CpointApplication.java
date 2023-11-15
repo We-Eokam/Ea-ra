a5c554b58a04064d3084a8a023b4151027a62704
이상재 <tkdwo7699@gmail.com>
2023-11-15 15:59:48 +0900

    Fix: JPAAuditing 추가

--- cpoint/src/main/java/com/eokam/cpoint/CpointApplication.java
+++ cpoint/src/main/java/com/eokam/cpoint/CpointApplication.java
@@ -3,13 +3,15 @@ package com.eokam.cpoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableJpaAuditing
 public class CpointApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CpointApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(CpointApplication.class, args);
+	}
 
 }
