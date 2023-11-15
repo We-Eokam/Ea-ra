45d318f7495e40ac6ff7a851d61165b85b740936
jxixeun <jxixeun@gmail.com>
2023-11-15 13:30:05 +0900

    :pencil2: Fix: 메시지 큐에 보내는 메시지가 snake_case로 가도록 설정

--- groo/src/main/java/com/eokam/groo/infrastructure/dto/RepayGrooRequest.java
+++ groo/src/main/java/com/eokam/groo/infrastructure/dto/RepayGrooRequest.java
@@ -2,6 +2,8 @@ package com.eokam.groo.infrastructure.dto;
 
 import com.eokam.groo.application.dto.GrooSavingDto;
 import com.eokam.groo.global.constant.SavingType;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,6 +14,7 @@ import lombok.ToString;
 @Getter
 @AllArgsConstructor
 @ToString
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class RepayGrooRequest {
 	private Long memberId;
 	private SavingType savingType;
