b2123d54cbd8ba19f8dce1fd7c762d1cb0c735a8
이상재 <tkdwo7699@gmail.com>
2023-11-16 18:02:56 +0900

    ✨Feat: 수신하는 MQ 구현

--- member/src/main/java/com/eokam/member/infra/mq/ReceiveMQService.java
+++ member/src/main/java/com/eokam/member/infra/mq/ReceiveMQService.java
@@ -3,7 +3,6 @@ package com.eokam.member.infra.mq;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import com.eokam.member.application.service.MemberService;
 import com.eokam.member.infra.dto.NotificationReqeust;
@@ -15,7 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class MQService {
+public class ReceiveMQService {
 
 	private final MemberService memberService;
 
@@ -26,15 +25,6 @@ public class MQService {
 	public void receiveMessage(RepayGrooRequest grooSavingRequest) {
 		memberService.repayGroo(grooSavingRequest.getMemberId(),grooSavingRequest.getSavingType(),grooSavingRequest.getGroo());
 		log.info("Data has been successfully saved to the GrooSaving. Saved Data: '{}'", grooSavingRequest);
-	}
-
-	public void sendFollowRequestNotification(NotificationReqeust request) {
-		template.convertAndSend("followRequestQueue", request);
-		log.info("Message {} sent successfully to queue followRequestQueue", request);
-	}
-	public void sendFollowAcceptNotification(NotificationReqeust request) {
-		template.convertAndSend("followAcceptQueue", request);
-		log.info("Message {} sent successfully to queue followAcceptQueue", request);
 	}
 
 }
