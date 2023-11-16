60a1e1fd54a2e571b37300ccbc23528e9def428f
양정훈 <yjhoon3658@naver.com>
2023-11-16 17:00:36 +0900

    :sparkles: Feat: 팔로우 관련 RebbbitMQ Listener 구현

--- notification/src/main/java/com/eokam/notification/infrastructure/rabbitmq/RabbitNotificationListener.java
+++ notification/src/main/java/com/eokam/notification/infrastructure/rabbitmq/RabbitNotificationListener.java
@@ -1,6 +1,5 @@
 package com.eokam.notification.infrastructure.rabbitmq;
 
-
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +8,7 @@ import com.eokam.notification.application.notification.service.NotificationServi
 import com.eokam.notification.application.token.service.FcmTokenService;
 import com.eokam.notification.infrastructure.fcm.service.FcmMessageService;
 import com.eokam.notification.presentation.dto.accusation.AccusationRequest;
+import com.eokam.notification.presentation.dto.follow.FollowRequest;
 import com.google.firebase.messaging.FirebaseMessagingException;
 
 import lombok.RequiredArgsConstructor;
@@ -36,4 +36,29 @@ public class RabbitNotificationListener {
 		log.info("Message sent successfully. Message: '{}'", notificationDto.getContent());
 	}
 
+	@RabbitListener(queues = "followRequestQueue")
+	public void receiveFollowMessage(FollowRequest followRequest) throws FirebaseMessagingException {
+		NotificationDto notificationDto = notificationService.saveFollowNotification(
+			NotificationDto.follow(followRequest));
+		log.info("Data has been successfully saved to the Notification. Saved Data: '{}'", notificationDto);
+		fcmMessageService.sendMessageTo(
+			fcmService.getToken(notificationDto.getReceiver()).token(),
+			"친구 요청",
+			notificationDto.getContent()
+		);
+		log.info("Message sent successfully. Message: '{}'", notificationDto.getContent());
+	}
+
+	@RabbitListener(queues = "followAcceptQueue")
+	public void receiveAcceptMessage(FollowRequest followRequest) throws FirebaseMessagingException {
+		NotificationDto notificationDto = notificationService.saveFollowAcceptNotification(
+			NotificationDto.followAccept(followRequest));
+		log.info("Data has been successfully saved to the Notification. Saved Data: '{}'", notificationDto);
+		fcmMessageService.sendMessageTo(
+			fcmService.getToken(notificationDto.getReceiver()).token(),
+			"친구 요청 수락",
+			notificationDto.getContent()
+		);
+		log.info("Message sent successfully. Message: '{}'", notificationDto.getContent());
+	}
 }
